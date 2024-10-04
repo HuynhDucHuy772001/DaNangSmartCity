5056735e2b78e816ec2dca5676ef7e8354aa2deb
HuynhDucHuy772001 <huynhd.huy77@gmail.com>
2024-10-04 10:28:07 +0700

    FINAL

--- src/Screens/Components/ListEv.js
+++ src/Screens/Components/ListEv.js
@@ -10,21 +10,66 @@ import { hp, wp } from '../../helpers/common';
 
 moment.locale('vi'); // Thiết lập ngôn ngữ mặc định là tiếng Việt
 
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-function ListEv({ dataEv, loading, onRefresh }) {
+
+const formatDate = (dateString) => {
+  const date = moment(dateString);
+  const day = date.date();
+  const month = date.month() + 1;
+  const year = date.year();
+  const dayOfWeek = capitalize(date.format('dddd')); // Lấy thứ trong tuần
+
+  return `${dayOfWeek}, ${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
+};
+const EventItem = React.memo(({ item, onPress }) => (
+  <Animated.View
+    entering={FadeInDown.delay(100).springify().damping(12)}
+    style={styles.content_list}>
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ width: wp(30), height: hp(12), alignSelf: 'center' }}
+    >
+      <Image
+        source={{ uri: item.hinh_anh }}
+        style={{ height: '100%', width: '90%', borderRadius: theme.radius.xx }}
+        resizeMode="cover"
+      />
+      {item.gia_ve.loai_gia_ve === 'Miễn phí' && (
+        <View style={styles.directionButton}>
+          <Text style={styles.textTicket}>{item.gia_ve.loai_gia_ve}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
+    <View style={{ width: wp(70) }}>
+      <TouchableOpacity onPress={onPress}>
+        <Text style={styles.textNameEvent} numberOfLines={2}>{item.ten_su_kien}</Text>
+      </TouchableOpacity>
+
+      <View style={styles.viewThoiGianSuKien}>
+        <Image source={require('../../assets/images/calender.png')} style={{ height: wp(7), width: wp(7) }} />
+        <View>
+          <Text style={styles.info_text} numberOfLines={2}>
+            {formatDate(item.ngay_dien_ra_su_kien)}
+          </Text>
+          <Text style={styles.info_text}>Bấm vào để xem thời gian chi tiết</Text>
+        </View>
+      </View>
+
+      <View style={styles.viewDiaDiem_LinhVuc}>
+        <Image source={require('../../assets/images/mappin.png')} style={{ height: wp(7), width: wp(7) }} />
+        <Text style={styles.info_text} numberOfLines={2}>{item.dia_diem.dia_chi}</Text>
+      </View>
+
+      <View style={[styles.viewDiaDiem_LinhVuc, { justifyContent: 'flex-end' }]}>
+        <Text style={{ color: theme.colors.main, fontSize: hp(1.8), fontFamily: 'Inter-ExtraBold' }}>{item.linh_vuc}</Text>
+      </View>
+    </View>
+  </Animated.View>
+));
+
+const ListEv = ({ dataEv, loading, onRefresh }) => {
   const navigation = useNavigation();
-
-  const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-
-  const formatDate = (dateString) => {
-    const date = moment(dateString);
-    const day = date.date();
-    const month = date.month() + 1;
-    const year = date.year();
-    const dayOfWeek = capitalize(date.format('dddd')); // Lấy thứ trong tuần
-
-    return `${dayOfWeek}, ${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
-  };
 
   // Sắp xếp các sự kiện theo ngày
   const sortedDataEv = dataEv.sort((a, b) => {
@@ -41,67 +86,27 @@ function ListEv({ dataEv, loading, onRefresh }) {
         <FlatList
           style={{ height: hp(100), width: wp(100), backgroundColor: "white" }}
           data={sortedDataEv}
-          renderItem={({ item, id }) => (
-            <Animated.View
-              entering={FadeInDown.delay(100).springify().damping(12)}
-              key={id}
-              style={styles.content_list}>
-              <TouchableOpacity
-                onPress={() => navigation.navigate("eventdetail", { event: item })}
-                style={{ width: wp(30), height: hp(12), alignSelf: 'center' }}
-              >
-                <Image
-                  source={{ uri: item.hinh_anh }}
-                  style={{ height: '100%', width: '90%', borderRadius: theme.radius.xx }}
-                  resizeMode="cover"
-                />
-                {item.gia_ve.loai_gia_ve === 'Miễn phí' ? (
-                  <View style={styles.directionButton}>
-                    <Text style={styles.textTicket}>{item.gia_ve.loai_gia_ve}</Text>
-                  </View>
-                ) : (
-                  null
-                )}
-              </TouchableOpacity>
-              <View style={{ width: wp(70) }}>
-                <TouchableOpacity onPress={() => navigation.navigate('eventdetail', { event: item })}>
-                  <Text style={styles.textNameEvent} numberOfLines={2}>{item.ten_su_kien}</Text>
-                </TouchableOpacity>
-
-                <View style={styles.viewThoiGianSuKien}>
-                  <Image source={require('../../assets/images/calender.png')} style={{ height: wp(7), width: wp(7) }} />
-                  <View>
-                    <Text style={styles.info_text} numberOfLines={2}>
-                      {formatDate(item.ngay_dien_ra_su_kien)}
-                    </Text>
-                    <Text style={styles.info_text}>Bấm vào để xem thời gian chi tiết</Text>
-                  </View>
-                </View>
-
-                <View style={styles.viewDiaDiem_LinhVuc}>
-                  <Image source={require('../../assets/images/mappin.png')} style={{ height: wp(7), width: wp(7) }} />
-                  <Text style={styles.info_text} numberOfLines={2}>{item.dia_diem.dia_chi}</Text>
-                </View>
-
-                <View style={[styles.viewDiaDiem_LinhVuc, { justifyContent: 'flex-end' }]}>
-                  <Text style={{ color: theme.colors.main, fontSize: hp(1.8), fontFamily: 'Inter-ExtraBold' }}>{item.linh_vuc}</Text>
-                </View>
-
-              </View>
-            </Animated.View>
-          )
-          }
+          renderItem={({ item }) => (
+            <EventItem
+              item={item}
+              onPress={() => navigation.navigate("eventdetail", { event: item })}
+            />
+          )}
+          keyExtractor={(item, index) => item?.id?.toString() || index.toString()}
+          initialNumToRender={10} // Số lượng phần tử ban đầu cần render
+          // windowSize={5} // Số lượng màn hình cần giữ trong bộ nhớ đệm
+          maxToRenderPerBatch={5} // Số lượng phần tử tối đa được render mỗi lần
           refreshControl={
-            < RefreshControl
+            <RefreshControl
               refreshing={loading}
               onRefresh={onRefresh}
             />
           }
         />
       )}
-    </View >
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -115,16 +120,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginVertical: '2%',
     marginHorizontal: '2%',
-    // backgroundColor: theme.colors.white,
-    // borderRadius: theme.radius.xx,
-    // shadowColor: '#000',
-    // shadowOffset: {
-    //   width: 0,
-    //   height: 2,
-    // },
-    // shadowOpacity: 0.25,
-    // shadowRadius: 3.84,
-    // elevation: 5,
     width: wp(96.5),
   },
   textTicket: {
